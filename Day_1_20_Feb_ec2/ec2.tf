c7a169ef647df18e4f1a9b3c793ca1aedcb2901d
ramkishor-537 <ramkishor.ch_007@outlook.com>
2026-03-16 13:10:45 +0530

    day1

---
+++ Day_1_20_Feb_ec2/ec2.tf
@@ -0,0 +1,39 @@
+# flow of execution for below code
+# first before executing aws_instance code terraform will create SG and get id of SG 
+# then after that create aws instance and attach SG through id
+
+resource "aws_instance" "example" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id] # .id means output we are getting from aws security group
+
+  tags = {
+    Name = "terraform-state-demo"
+    Project = "roboshop"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow-all-terraform" # this is for AWS account
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow-all-terraform"
+  }
+}
